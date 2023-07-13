ce0534654254b84069086624f83dcdc3cf95c506
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-07-13 18:40:47 +0530

    initial commit

---
+++ springboot-backend/src/main/java/com/management/proj/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.management.proj.controller;
+
+import com.management.proj.model.Employee;
+import com.management.proj.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
---
+++ springboot-backend/src/main/java/com/management/proj/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.management.proj.repository;
+
+import com.management.proj.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
