35d0e7b5a7b637e011faa305c930a9d273342141
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-07-17 21:49:03 +0530

    added comments

--- reactjs-frontend/src/services/EmployeeService.js
+++ reactjs-frontend/src/services/EmployeeService.js
@@ -3,22 +3,27 @@ import axios from 'axios'
 const EMPLOYEE_API_BASE_URL = 'http://localhost:8080/api/v1/employees'
 
 class EmployeeService {
+  //get employees
   getEmployees() {
     return axios.get(EMPLOYEE_API_BASE_URL)
   }
 
+  //post employees
   saveEmployee(employee) {
     return axios.post(EMPLOYEE_API_BASE_URL, employee)
   }
 
+  //get employee by id
   getEmployeeById(id) {
     return axios.get(EMPLOYEE_API_BASE_URL + `/${id}`)
   }
 
+  //update employee by id
   updateEmployee(id, employee) {
     return axios.put(EMPLOYEE_API_BASE_URL + `/${id}`, employee)
   }
 
+  //delete employee by id
   deleteEmployee(id) {
     return axios.delete(EMPLOYEE_API_BASE_URL + `/${id}`)
   }
